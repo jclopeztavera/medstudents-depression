064f2cb38743efdfc51dbee5314c3e81dd4c528c
Juan C. López-Tavera <jc.lopeztavera@gmail.com>
2017-02-07 02:27:43 -0600

    minor advances in modeling script
    set up initial hypothesis and first model fitting

--- R/alt-approach-modele.R
+++ R/alt-approach-modele.R
@@ -2,13 +2,12 @@
 # are independent. The glmer() function should be used for correlated binary responses. Only
 # binary predictors are allowed. If categorization for predictors is other than (0,1), factor() should
 # be considered.
-if (sum(c("boot", "lme4") %in% installed.packages()) > 0) {
-  install.packages(c("boot", "lme4"))
+if (!require(lme4)) {
+  install.packages("lme4")
 }
-library(boot)
 library(lme4)
 
-source("medstuds-depranx-dumpeddata.R")
+source('~/GitHub/medstudents-depression/data/medstuds-depranx-dumpeddata.R')
 
 attach(data)
 
@@ -18,10 +17,34 @@ sub <- sample(nrow(data), floor(nrow(data) * 0.8))
 train <- data[sub,]
 test <- data[-sub,]
 
-b.hyp1 <- formula(depression ~ bmi + (1 | who.bmi)) # hypothesis 1
-b.hyp1.1 <- formula(suicidal ~ who.bmi) # hypothesis 1.1
-b.hyp2 <- formula(anxiety ~ who.bmi) # hypothesis 2
+
+b.hyp1 <-
+  formula(depression ~ who.bmi + (1 | who.bmi)) # hypothesis 1
+b.hyp1.1 <-
+  formula(suicidal ~ who.bmi + (1 | who.bmi)) # hypothesis 1.1
+b.hyp2 <- formula(anxiety ~ who.bmi + (1 | who.bmi)) # hypothesis 2
 
 
-glmer(formula = b.hyp1, data = train, family = binomial(link = "logit"), nAGQ = 9) %>% plot
+bh1.model <-
+  glmer(
+    formula = b.hyp1,
+    data = train,
+    family = binomial(link = "logit"),
+    nAGQ = 20
+  )
 
+bh1.1model <-
+  glmer(
+    formula = b.hyp1.1,
+    data = train,
+    family = binomial(link = "logit"),
+    nAGQ = 20
+  )
+
+bh2.model <-
+  glmer(
+    formula = b.hyp2,
+    data = train,
+    family = binomial(link = "logit"),
+    nAGQ = 20
+  )
