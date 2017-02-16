453c6af694380cb8136738009daf982a174a056e
Juan C. López-Tavera <jc.lopeztavera@gmail.com>
2017-02-16 13:25:45 -0600

    Updated visuals
    Added density plots for depression and anxiety, scaled by severity
    measure

--- R/visualize.R
+++ R/visualize.R
@@ -3,11 +3,48 @@
 # This code is licensed under a MIT License Copyright (c) 2016 Juan C. López-Tavera
 
 source('~/github/medstudents-depression/R/loader.R')
-loader("VIM")
+source('~/github/medstudents-depression/data/processed/medstudents-depression.R')
+loader("VIM", "ggplot2", "readr", "ggthemes")
 
 ## visualizing missing data
-raw_data <- read_csv("~/github/medstudents-depression/data/raw/medstuds-depranx-data.csv")
-names(raw_data) <- c("School-year", "Age", "Binary gender", "Weight", "Height", "PHQ-1", "PHQ-2", "PHQ-3", "PHQ-4", "PHQ-5", "PHQ-6", "PHQ-7", "PHQ-8", "PHQ-9", "Prior depression", "Prior anti-depressives", "GAD-1", "GAD-2", "GAD-3", "GAD-4", "GAD-5", "GAD-6", "GAD-7", "Prior anxiety", "Prior anxiolytics", "Epworth 1", "Epworth 2", "Epworth 3", "Epworth 4", "Epworth 5", "Epworth 6", "Epworth 7", "Epworth 8")
+raw_data <-
+  read_csv("~/github/medstudents-depression/data/raw/medstudents-depression-raw.csv")
+names(raw_data) <-
+  c(
+    "School-year",
+    "Age",
+    "Binary gender",
+    "Weight",
+    "Height",
+    "PHQ-1",
+    "PHQ-2",
+    "PHQ-3",
+    "PHQ-4",
+    "PHQ-5",
+    "PHQ-6",
+    "PHQ-7",
+    "PHQ-8",
+    "PHQ-9",
+    "Prior depression",
+    "Prior anti-depressives",
+    "GAD-1",
+    "GAD-2",
+    "GAD-3",
+    "GAD-4",
+    "GAD-5",
+    "GAD-6",
+    "GAD-7",
+    "Prior anxiety",
+    "Prior anxiolytics",
+    "Epworth 1",
+    "Epworth 2",
+    "Epworth 3",
+    "Epworth 4",
+    "Epworth 5",
+    "Epworth 6",
+    "Epworth 7",
+    "Epworth 8"
+  )
 
 par(lty = 0)
 md_plot <-
@@ -22,3 +59,30 @@ md_plot <-
     ylab = c("Histogram of missing data", "Pattern of missing data")
   )
 
+ddp <-
+  ggplot(
+    data = data,
+    mapping = aes(x = phq.score, fill = phq.scale, colour = phq.scale)
+  )
+ddp <- ddp +
+  geom_density(alpha = 0.5) +
+  theme_bw() +
+  scale_fill_ptol(name = "Depression Severity") +
+  scale_color_ptol(name = "Depression Severity") +
+  labs(
+    list(title = "Distribution of PHQ-9 Scores by Depression Severity", x = "PHQ-9 Score", y = "Density")
+  ) +
+  expand_limits(x = c(0,27))
+
+gdp <-  ggplot(
+    data = data,
+    mapping = aes(x = gad.score, fill = gad.scale, colour = gad.scale)
+  )
+
+gdp <- gdp +
+  geom_density(alpha = 0.5) + theme_bw() +
+  scale_fill_ptol(name = "Anxiety Severity") +
+  scale_color_ptol(name = "Anxiety Severity") +
+  labs(
+    list(title = "Distribution of GAD-7 Scores by Anxiety Severity", x = "GAD-7 Score", y = "Density")
+  ) + expand_limits(x = c(0,21))
